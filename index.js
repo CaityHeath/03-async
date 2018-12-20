2dd7cab55a14284a2d56793a9536857739432f72
CaityHeath <caity.heath@gmail.com>
2018-12-19 19:32:01 -0800

    all tests pass and the reader-fixed fills the contents array as expected

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 'use strict';
-
-const fileReader = require('./lib/reader.js');
-
+const fs = require('fs');
+const fileReader = require('./lib/reader-fixed.js');
+console.log(fileReader);
 // Obtain and assert input
-let files = process.argv[2];
+let files = process.argv.slice(2);
 console.log(files);
 
 
-// if( ! (files instanceof Array && files.length) ) {
-//   throw new Error('Invalid Args');
-// }
+if( ! (files instanceof Array && files.length) ) {
+  throw new Error('Invalid Args');
+}
 
 fileReader(files, (err,data) => {
+  console.log(data);
+  console.log(`in fileReader`);
   if ( err ) { throw err; }
   console.log('From Callback:', data);
+  
 });
