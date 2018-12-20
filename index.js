c4305f5de4f80eaa6d9677c08ce0241803adfd7a
CaityHeath <caity.heath@gmail.com>
2018-12-19 20:26:40 -0800

    added readme

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 'use strict';
-const fs = require('fs');
+
 const fileReader = require('./lib/reader-fixed.js');
 console.log(fileReader);
 // Obtain and assert input
 let files = process.argv.slice(2);
-console.log(files);
 
 
 if( ! (files instanceof Array && files.length) ) {
@@ -12,9 +11,6 @@ if( ! (files instanceof Array && files.length) ) {
 }
 
 fileReader(files, (err,data) => {
-  console.log(data);
-  console.log(`in fileReader`);
   if ( err ) { throw err; }
   console.log('From Callback:', data);
-  
 });
