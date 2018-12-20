beaeacb8ea38db59b0464f7dcc80c072f0169145
CaityHeath <caity.heath@gmail.com>
2018-12-19 17:25:41 -0800

    beginning of Assignment 2 complete: tests won't pass

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 const fileReader = require('./lib/reader.js');
 
 // Obtain and assert input
-let files = process.argv.slice(2);
+let files = process.argv[2];
+console.log(files);
 
-if( ! (files instanceof Array && files.length) ) {
-  throw new Error('Invalid Args');
-}
+
+// if( ! (files instanceof Array && files.length) ) {
+//   throw new Error('Invalid Args');
+// }
 
 fileReader(files, (err,data) => {
   if ( err ) { throw err; }
